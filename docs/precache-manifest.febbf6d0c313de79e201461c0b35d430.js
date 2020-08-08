bdc5d37b111e6dfe6b7db99fb1363e5989158062
Majid Burney <moocow@euphoricsoup.com>
2020-08-08 14:40:16 -0700

    github pages base dir

---
+++ docs/precache-manifest.febbf6d0c313de79e201461c0b35d430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d3b7df483c952f1f9d713e075d5166",
+    "url": "/battlemap/index.html"
+  },
+  {
+    "revision": "7a1f52d6b207fa6e2512",
+    "url": "/battlemap/static/css/main.a7235e1d.chunk.css"
+  },
+  {
+    "revision": "06fd9f9b8e7cf66be515",
+    "url": "/battlemap/static/js/2.249505b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battlemap/static/js/2.249505b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1f52d6b207fa6e2512",
+    "url": "/battlemap/static/js/main.8966974c.chunk.js"
+  },
+  {
+    "revision": "857fdedd7928d6557d1b",
+    "url": "/battlemap/static/js/runtime-main.06a7faa8.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8fd9a1118990e4371ca23f95ab4f8d6.js"
+  "/battlemap/precache-manifest.febbf6d0c313de79e201461c0b35d430.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/battlemap/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
